8faf72eae77498518de8a220131f61dc9f68a259
omarallabadi <omarallabadi197@gmail.com>
2026-03-24 05:09:36 +0300

    second stage navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
       <main >
-      <Button className="bg-red-600">hello</Button>
+ 
       </main>
     
   );
--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 import { Routes } from "@/app/constants/enums";
-import { link } from "fs";
 import Link from "next/link";
-import { title } from "process";
-import React from "react";
+import { useState } from "react";
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
 
 function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
   const Links = [
-    { id: crypto.randomUUID(), title: "Menu", href: Routes.MENU },
+    {id: crypto.randomUUID(), title: "Menu", href: Routes.MENU },
     { id: crypto.randomUUID(), title: "About", href: Routes.ABOUT },
-    { id: crypto.randomUUID(), title: "Contact", href: Routes.CONTACT },
+    {  id: crypto.randomUUID(), title: "Contact", href: Routes.CONTACT },
     {
       id: crypto.randomUUID(),
       title: "Login",
@@ -16,13 +20,42 @@ function Navbar() {
     },
   ];
   return (
-    <nav>
-      <ul className="flex gap-9 my-4 items-center font-semibold ">
+    <nav className="py-10">
+      <Button
+        variant="secondary"
+        size="sm"
+        className="md:hidden"
+        onClick={() => {
+          setOpenMenu(true);
+        }}
+      >
+        <Menu />
+      </Button>
+
+      <ul
+        className={`fixed top-0  flex flex-col bg-background h-full  w-full md:static ${openMenu ? "left-0 z-50" : "-left-full"}    md:flex  md:flex-row gap-9  items-center transition-all duration-300  font-semibold `}
+      >
+        <section className=" w-full py-5 justify-end px-10 flex">
+        <Button
+          variant="secondary"
+          size="sm"
+          className="md:hidden rounded-full px-2 py-1"
+          onClick={() => {
+            setOpenMenu(false);
+          }}
+        >
+          <X />
+        </Button>
+        </section>
         {Links.map((link) => (
-           
-          <Link className={`${  link.title==="Login"?"bg-primary rounded-3xl px-6 py-2 text-white": "text-accent hover:text-primary font-bold duration-300 transition-colors"}`} key={link.id} href={`/${link.href}`}>
-            {link.title}
-          </Link>
+          <li key={link.id}>
+            <Link
+              className={`${link.title === "Login" ? "bg-primary rounded-3xl px-6 py-2 text-white" : "text-accent hover:text-primary font-bold duration-300 transition-colors"}`}
+              href={`/${link.href}`}
+            >
+              {link.title}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
